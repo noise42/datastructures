a921afe07be4da338b99f945e7a9adb3fbb2d1b7
Marco Pietrosanto <marco23.p@gmail.com>
2017-09-01 15:37:15 +0200

    started course repository, hope I will manage to keep it sane and trashless

---
+++ materials/zebra/zebra.py
@@ -0,0 +1,39 @@
+import itertools
+import time
+def imright(right,left):
+	return 1==(right-left)
+
+def nextto(A,B):
+	return 1==(A-B) or 1==(B-A)
+
+def zebra_puzzle():
+	houses = first, _ , middle, _, _ = [1,2,3,4,5]
+	orderings = list(itertools.permutations(houses))
+	#print orderings
+	solution=[(WATER, ZEBRA) 
+					for (red,green,ivory,yellow,blue) in orderings
+					for (Englishman,Spaniard,Ukrainian,Japanese,Norwegian) in orderings
+					for (dog,snails, fox, horse, ZEBRA) in orderings
+					for (coffee, tea, milk, oj, WATER) in orderings
+					for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+					if Englishman is red
+					if Spaniard is dog
+					if coffee is green
+					if Ukrainian is tea
+					if imright(green,ivory)
+					if OldGold is snails
+					if Kools is yellow
+					if milk is middle
+					if Norwegian is first
+					if nextto(Chesterfields, fox)
+					if nextto(Kools, horse)
+					if LuckyStrike is oj
+					if Japanese is Parliaments
+					if nextto(Norwegian,blue)
+					]
+	return solution
+
+t0=time.time()
+print zebra_puzzle()
+t1=time.time()
+print str(t1-t0) + " s"
---
+++ materials/zebra/zebraOpt.py
@@ -0,0 +1,44 @@
+import itertools
+import time
+
+def after(right,left):
+	return 1==(right-left)
+
+def nextto(A,B):
+	return 1==(A-B) or 1==(B-A)
+
+def zebra_puzzle():
+	houses = first, _ , middle, _, _ = [1,2,3,4,5]
+	orderings = list(itertools.permutations(houses))
+	#print orderings
+	solution=[(WATER, ZEBRA) 
+					for (red,green,ivory,yellow,blue) in orderings
+					if after(green,ivory)# is green after ivory?
+
+					for (Englishman,Spanish,Ukrainian,Japanese,Norwegian) in orderings
+					if Englishman is red#
+					if Norwegian is 1 #
+					if nextto(Norwegian,blue) #
+
+					for (dog,snails, fox, horse, ZEBRA) in orderings
+					if Spanish is dog #
+
+					for (coffee, tea, milk, rum, WATER) in orderings
+					if milk is 3 #
+					if coffee is green#
+					if Ukrainian is tea #
+
+					for (Metal, Classical, Rock, Country, Pop) in orderings
+					if Metal is snails	#				
+					if Classical is yellow					
+					if nextto(Rock, fox)#
+					if nextto(Classical, horse)#
+					if Country is rum #
+					if Japanese is Pop #
+					
+					]
+	return solution
+t0=time.time()
+print zebra_puzzle()
+t1=time.time()
+print str(t1-t0) + "s"
